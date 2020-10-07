45998262a7d9a90a9373e7220059777031366bb7
gilc1870 <gilc1870@wrdsb.ca>
2020-10-07 15:31:38 +0000

    This will determine if the number you input is even or odd

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-/**
+import java.util.Scanner;
+
+
+/** This will determine if the number you input is even or odd
  *
- * @author 
+ * @author Cesar Gil
  */
 public class Main {
 
@@ -10,6 +13,20 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+
+  
+  System.out.println("Please input a Number to determind if it's even or odd:");
+Scanner input = new Scanner(System.in);
+int integer = input.nextInt();
+
+if (integer % 2 == 0){
+System.out.println("This integer is even");
+
+} else if (integer % 2 == 1){
+  System.out.println("This integer is odd");
+}
+
+
+
   }
 }
